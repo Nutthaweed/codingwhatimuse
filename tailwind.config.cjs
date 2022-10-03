b9ebb033567254d8421ba903b5ccc1a26f039e91
Nutthaweed <97228092+Nutthaweed@users.noreply.github.com>
2022-10-03 14:26:56 +0700

    commits

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
 		extend: {
 			fontFamily: {
-				Varela: [" 'Varela Round' ",'Verdana' , 'sans-serif']
+				varela: [" 'Varela Round' ",'Verdana' , 'sans-serif']
 			}
 		},
 	},
